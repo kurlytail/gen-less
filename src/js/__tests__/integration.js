7518a6a5dd7a4165541c6b339e9958cd5c343926
ashwin <ashwinkn@hotmail.com>
2019-03-20 11:50:30 -0500

    generated by     - gen-lib@1.1.3     - @kurlytail/gen-sgen@1.1.6

---
+++ src/js/__tests__/integration.js
@@ -0,0 +1,23 @@
+import { execSync } from 'child_process';
+
+describe('# integration test', () => {
+    beforeEach(() => {
+        execSync('rm -rf testoutput');
+    });
+
+    it('## should print help options', () => {
+        let output = execSync('npm run build').toString();
+        output = execSync('sgen -g `pwd`/dist/less.min.js -h').toString();
+        expect(output).toMatchSnapshot();
+    });
+
+    it('## should generate design and run less commands', () => {
+        let output = execSync('npm run build').toString();
+        output = execSync('sgen -g `pwd`/dist/less.min.js -d src/test/fixture/design.json -o testoutput').toString();
+        output = output.replace(/info: Loaded generator .*less.min.js.*/, '');
+        expect(output).toMatchSnapshot();
+        output = execSync('npm install', { cwd: 'testoutput' }).toString();
+        output = execSync('npm run lint', { cwd: 'testoutput' }).toString();
+        output = execSync('npm run build', { cwd: 'testoutput' }).toString();
+    });
+});
